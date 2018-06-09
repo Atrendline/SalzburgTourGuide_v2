6f7a8e61472f0e96ae91d09d7d2e142101d360c0
Your name <you@example.com>
2018-06-09 22:48:36 +0200

    Second commit

--- app/src/main/java/com/example/android/salzburgtourguide/MainActivity.java
+++ app/src/main/java/com/example/android/salzburgtourguide/MainActivity.java
@@ -8,32 +8,17 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-/**
- * Created by Atrendline
- */
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private SectionsPagerAdapter SectionsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
-    private ViewPager mViewPager;
+    private ViewPager ViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,20 +29,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar( toolbar );
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter( getSupportFragmentManager() );
+        SectionsPagerAdapter = new SectionsPagerAdapter( getSupportFragmentManager() );
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById( R.id.container );
-        mViewPager.setAdapter( mSectionsPagerAdapter );
+        ViewPager = (ViewPager) findViewById( R.id.container );
+        ViewPager.setAdapter( SectionsPagerAdapter );
 
-        TabLayout tabLayout = (TabLayout) findViewById( R.id.tabs );
+        TabLayout tabLayout = findViewById( R.id.tabs );
 
-        mViewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener( tabLayout ) );
-        tabLayout.addOnTabSelectedListener( new TabLayout.ViewPagerOnTabSelectedListener( mViewPager ) );
-
-
+        ViewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener( tabLayout ) );
+        tabLayout.addOnTabSelectedListener( new TabLayout.ViewPagerOnTabSelectedListener( ViewPager ) );
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,41 +64,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt( ARG_SECTION_NUMBER, sectionNumber );
-            fragment.setArguments( args );
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate( R.layout.fragment_main, container, false );
-            TextView textView = (TextView) rootView.findViewById( R.id.section_label );
-            textView.setText( getString( R.string.section_format, getArguments().getInt( ARG_SECTION_NUMBER ) ) );
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -135,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
                     return new Hotels();
                 case 2:
                     return new Dining();
+                case 3:
+                    return new Events();
                 default:
                     return null;
             }
@@ -142,8 +91,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 4 total pages.
+            return 4;
         }
     }
 }
